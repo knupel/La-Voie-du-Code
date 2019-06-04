80ce617ab90d65c5cbac3104b4e8f60a6930efa6
Stan le Punk <stanislasiii@gmail.com>
2019-06-04 15:39:29 +0200

    re-add ECV typescript with an updated git ignor for node module

---
+++ 2019_6_4_ECV/ecv_typescript/src/sketch.ts
@@ -0,0 +1,57 @@
+// import vec from './Z_vec'
+// import vec2 from './Z_vec2'
+// import vec3 from './Z_vec3'
+// import vec4 from './Z_vec4'
+
+import vec from './vector/Z_vec'
+import vec2 from './vector/Z_vec2'
+import vec3 from './vector/Z_vec3'
+import vec4 from './vector/Z_vec4'
+
+
+
+var sketch = (p: p5) => {
+  p.preload = () => {
+
+  }
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+    // p.colorMode(p.HSB,360,100,100,100);
+    w = p.windowWidth;
+    h = p.windowHeight;
+  }
+
+  p.windowResized = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+    w = p.windowWidth;
+    h = p.windowHeight;
+  }
+  
+  p.draw = () => {
+    p.background(255,0,0);
+    let px: number = p.mouseX;
+    let py: number = p.mouseY;
+    pos.set(px,py);
+    pos.sub(w/2,h/2);
+    p.fill(0,255,0);
+    p.ellipse(pos.x(),pos.y(),50,50);
+  }
+}
+
+// var sketchP = new p5(sketch);
+
+var test = new p5(sketch);
+
+let w : number = 0;
+let h : number = 0;
+
+let pos = new vec2(0,0);
+
+
+
+
+
+
+
+
